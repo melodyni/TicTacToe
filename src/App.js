884bf172a853e4445267bec73ab9a45f9b703827
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-08-26 21:18:34 +0530

    reloading page on click

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import Game from './Game';
 
+const newGame = () => {
+  window.location.reload(false);
+};
+
 const App = () => {
-  return <Game />;
+  return (
+    <div className='container'>
+      <Game />
+      <br />
+      <button onClick={newGame} className='font button '>
+        New Game
+      </button>
+    </div>
+  );
 };
 
 export default App;
