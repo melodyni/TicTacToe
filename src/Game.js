c29927734b8b23d07dcbcf964e2d3c8cefd04879
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-08-26 19:25:27 +0530

    modified status showing in game

--- src/Game.js
+++ src/Game.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import Grid from './Grid';
-import Status from './Status';
 
+const Status = (props) => <p className='font'>{props.msg}</p>;
+
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { player: 'X', cells: Array(9).fill(null), winner: null };
+    this.state = { player: 'X', cells: Array(9).fill(null), status: 'turn' };
     this.updateGame = this.updateGame.bind(this);
+  }
+
+  getGameStatus(cells) {
+    const isGameWon = winConditions.some((subCell) =>
+      subCell.every((id) => cells[id] === this.state.player)
+    );
+    const allMovePlayed = cells.every((c) => c);
+    return isGameWon ? 'won' : allMovePlayed ? 'draw' : 'turn';
   }
 
   updateGame(event) {
     const id = event.target.id;
-    const { cells, winner, player } = this.state;
+    const { cells, status, player } = this.state;
 
-    if (!cells[id] && !winner) {
+    if (!cells[id] && status === 'turn') {
       let newCells = cells.slice();
       newCells[id] = player;
 
-      const isGameWon = this.props.winConditions.some((cells) =>
-        cells.every((id) => newCells[id] === player)
-      );
-
       this.setState({
-        winner: isGameWon ? player : null,
+        status: this.getGameStatus(newCells),
         player: player === 'X' ? '0' : 'X',
         cells: newCells,
       });
@@ -30,12 +45,15 @@ class Game extends React.Component {
   }
 
   render() {
-    const { winner, player, cells } = this.state;
-    const winMsg = <Status message={`Winner: ${winner}`} />;
-    const turnMsg = <Status message={`Turn: Player ${player}`} />;
+    const { status, player, cells } = this.state;
+    const statusMsg = {
+      won: `Winner: ${player === 'X' ? '0' : 'X'}`,
+      turn: `Turn: Player ${player}`,
+      draw: `Draw Game`,
+    };
     return (
       <div>
-        {winner ? winMsg : turnMsg}
+        <Status msg={statusMsg[status]} />
         <Grid cells={cells} onClick={this.updateGame} />
       </div>
     );
