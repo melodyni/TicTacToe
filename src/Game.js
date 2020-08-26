6e52ec8d15b0739d6af973a380007dfb11941c25
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-08-26 10:10:32 +0530

    added declaring winner feature and ending the game

--- src/Game.js
+++ src/Game.js
@@ -4,24 +4,58 @@ import Grid from './Grid';
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    this.winner = null;
     this.state = { player: 'X', cells: Array(9).fill(null) };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(event) {
-    const cellId = event.target.id;
-    if (this.state.cells[cellId] === null) {
-      let newCells = this.state.cells.slice();
-      newCells[cellId] = this.state.player;
-      this.setState({
-        player: this.state.player === 'X' ? '0' : 'X',
-        cells: newCells,
-      });
+  declareWinner() {
+    console.log('in decla');
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const [strike] = winConditions.filter(([a, b, c]) => {
+      return (
+        this.state.cells[a] &&
+        this.state.cells[a] === this.state.cells[b] &&
+        this.state.cells[a] === this.state.cells[c]
+      );
+    });
+    if (strike) {
+      this.winner = this.state.cells[strike[0]];
+      return this.state.cells[strike[0]];
     }
   }
 
+  handleClick(event) {
+    console.log('in handle');
+    const cellId = event.target.id;
+    if (this.state.cells[cellId] || this.winner) {
+      return;
+    }
+    let newCells = this.state.cells.slice();
+    newCells[cellId] = this.state.player;
+    this.setState({
+      player: this.state.player === 'X' ? '0' : 'X',
+      cells: newCells,
+    });
+  }
+
   render() {
-    return <Grid cellIds={this.state.cells} onClick={this.handleClick} />;
+    console.log('in render', this.state, this.winner);
+    return (
+      <div>
+        <p className='status'>Winner: {this.declareWinner()}</p>
+        <Grid cellIds={this.state.cells} onClick={this.handleClick} />
+      </div>
+    );
   }
 }
 
