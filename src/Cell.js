84c76db3749cfd0cec54b208c306c5542f041f5c
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-08-26 20:47:26 +0530

    added colors on X and O

--- src/Cell.js
+++ src/Cell.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './game.css';
 
 const Cell = (props) => {
+  const { symbol, id, onClick } = props;
+  let color = '';
+  if (symbol === 'X') {
+    color = 'red';
+  } else if (symbol === '0') {
+    color = 'blue';
+  }
+  const styles = `cell font ${color}`;
   return (
-    <button className='cell font' id={props.id} onClick={props.onClick}>
-      {props.symbol}
+    <button className={styles} id={id} onClick={onClick}>
+      {symbol}
     </button>
   );
 };
