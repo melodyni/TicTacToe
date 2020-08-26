9527aa93b4c7ca87b43113da23d0e2ebd6550874
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-08-26 11:22:55 +0530

    checking win conditions in handleClick
    passing winCondition as props to game
    removed declareWinner function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Game from './Game';
 
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const App = () => {
-  return <Game />;
+  return <Game winConditions={winConditions} />;
 };
 
 export default App;
--- src/Game.js
+++ src/Game.js
@@ -4,55 +4,34 @@ import Grid from './Grid';
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.winner = null;
-    this.state = { player: 'X', cells: Array(9).fill(null) };
+    this.state = { player: 'X', cells: Array(9).fill(null), winner: null };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  declareWinner() {
-    console.log('in decla');
-    const winConditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    const [strike] = winConditions.filter(([a, b, c]) => {
-      return (
-        this.state.cells[a] &&
-        this.state.cells[a] === this.state.cells[b] &&
-        this.state.cells[a] === this.state.cells[c]
-      );
-    });
-    if (strike) {
-      this.winner = this.state.cells[strike[0]];
-      return this.state.cells[strike[0]];
-    }
-  }
-
   handleClick(event) {
-    console.log('in handle');
-    const cellId = event.target.id;
-    if (this.state.cells[cellId] || this.winner) {
-      return;
+    const id = event.target.id;
+    const { cells, winner, player } = this.state;
+
+    if (!cells[id] && !winner) {
+      let newCells = cells.slice();
+      newCells[id] = player;
+
+      const isGameWon = this.props.winConditions.some((cells) =>
+        cells.every((id) => newCells[id] === player)
+      );
+
+      this.setState({
+        winner: isGameWon ? player : null,
+        player: player === 'X' ? '0' : 'X',
+        cells: newCells,
+      });
     }
-    let newCells = this.state.cells.slice();
-    newCells[cellId] = this.state.player;
-    this.setState({
-      player: this.state.player === 'X' ? '0' : 'X',
-      cells: newCells,
-    });
   }
 
   render() {
-    console.log('in render', this.state, this.winner);
     return (
       <div>
-        <p className='status'>Winner: {this.declareWinner()}</p>
+        <p className='status'>Winner: {this.state.winner}</p>
         <Grid cellIds={this.state.cells} onClick={this.handleClick} />
       </div>
     );
