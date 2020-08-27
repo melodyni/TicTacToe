00d3449159f5a1b1e27ac90116bca8f4b28a415f
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-08-27 10:08:11 +0530

    build 1

---
+++ build/precache-manifest.a523ceee6f400c2798df36032aed1159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc813f14537f38c937e3ab994d69905e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72e010d3c1b9bf00e260",
+    "url": "/static/css/main.f3d7325d.chunk.css"
+  },
+  {
+    "revision": "199d6fa0cb0b8e8b3985",
+    "url": "/static/js/2.1458acff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1458acff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e010d3c1b9bf00e260",
+    "url": "/static/js/main.0432359c.chunk.js"
+  },
+  {
+    "revision": "df40ba753f8222cfb8ed",
+    "url": "/static/js/runtime-main.0cd3ba68.js"
+  }
+]);
